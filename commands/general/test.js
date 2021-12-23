60ec3e7d75aa561e45693e9f4e3e6debdcd74ef8
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2021-12-23 17:54:46 +0100

    ok devo aggiungere la cartela events

--- commands/general/test.js
+++ commands/general/test.js
@@ -2,6 +2,7 @@ module.exports = {
     name:"test",
     description:"Test command",
     aliases: ["test2"],
+    onlyStaff: true,
     execute(message, args) {
         message.channel.send("Test command!");
     }
--- index.js
+++ index.js
@@ -9,51 +9,69 @@ client.on ('ready', () => {
 });
 
 
-//*  Richiedo al diperndenza fs */
+//*  Richiedo al diperndenza fs 
+// I require FS dependency 
+
 const fs = require('fs');
 
 //** Creo una collection */
+// I create a collection
 client.commands = new Discord.Collection();
 
 //** Creo una collezione di comandi solo nella cartella /commands*/
+// I create a collection of commands only in the /commands folder
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 //** Per ogni file, eseguo il require */
+// For each file, I execute the require
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
 
 /** Creo una collezione di comandi per tutte le cartelle di commands*/
+// I create a collection of commands for all the commands folders
 const commandsFolder = fs.readdirSync('./commands')
 for (const folder of commandsFolder) {
     const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
     for (const file of commandFiles) {
         const command = require(`./commands/${folder}/${file}`);
         //** Aggiungo il comando alla collezione */
+        // I add the command to the collection
         client.commands.set(command.name, command);
     }
 }
 
 //** Eseguo il comando */
+// I run the command
 client.on('messageCreate', message => {
     if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
 
     //** Prendo il comando */
+    // I get the command1
     const args = message.content.slice(process.env.PREFIX.length).split(/ +/);
     const commandName = args.shift().toLowerCase();
 
     //** Controllo il nome del comando e i suoi alias */
+    // I check the command name and its aliases
     if (!client.commands.has(commandName) && !client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName)))return;
 
     const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
+    if (command.onlyStaff) {
+        if (!message.member.roles.cache.find(r => r.name === "Red Lettuce") || !message.member.roles.cache.find(r => r.name === "Avocado (MOD)")) {
+            return message.channel.send(`You don't have the permission to use this command!`);
+        }
+    }
+
     //** Controllo se il comando è satato inviato nei DM  */
+    // I check if the command is sent in DM
     if (command.guildOnly && message.channel.type !== 'text') {
         return message.reply('I can\'t execute that command inside DMs!');
     }
 
     //** Controllo se il comando è stato inviato in un canale di supporto */
+    // I check if the command is sent in a support channel
     try {
         command.execute(message, args);
     } catch (error) {
