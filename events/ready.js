6573be5f75d97d823b2f2327fd5e54abb1c04955
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2021-12-26 22:24:24 +0100

    non riesco a facciare un dato da una api per il prezzo degli eth da incollare nella chat vocale

--- events/ready.js
+++ events/ready.js
@@ -8,5 +8,14 @@ module.exports = {
 
         // Set the bot's status
         client.user.setActivity('we have no funds, yes this doodles is a screanshot LOL', { type: 'WATCHING' });
-    }
+
+        // Price etereum in EUR 
+
+        // setInterval(async function() {
+        //     var channel = client.channels.cache.get('923557234351419402')
+        //     await fetch("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=eur")
+        //         .then(res => res.json())
+        //         .then(data => console.log(data));
+        // }, 1000 * 5);
+    } 
 }
